39297c155756ff03a07f2f5dd3a87042bfde6c5c
Jigar Tarpara <jigartarpara68@gmail.com>
2018-05-06 17:17:45 +0530

    Gross Profit Report

---
+++ report_gross_profit/gross_profit_customization/report/gross_profit_customize/gross_profit_customize.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2016, Jigar Tarpara and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Gross Profit Customize"] = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 1,
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date")
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date")
+		},
+		{
+			"fieldname":"group_by",
+			"label": __("Group By"),
+			"fieldtype": "Select",
+			"options": "Invoice\nItem Code\nItem Group\nBrand\nWarehouse\nCustomer\nCustomer Group\nTerritory\nSales Person\nProject\nItem Code and Customer",
+			"default": "Invoice"
+		},
+	]
+}
